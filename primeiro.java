6741b03c8c5300d74c48a452b40a518db4a6377d
Muryllo <muryllo.henrique@hotmail.com>
2023-06-03 17:15:55 -0300

    Initial commit

---
+++ primeiro.java
@@ -0,0 +1,23 @@
+// nome da classe
+class MeuPrimeiroProgramaJava
+{
+    //módulo principal com a entrada pela linha de comando
+    public static void main (String entrada[])
+    {
+        //declaração de variáveis
+        String frase = "Muryllo Henrique";
+        String curso = "Ciência da Computação";
+        int inteiro = 20;
+        char caracter = 'M';
+        double peso = 57.5;
+        double real = 1.84;
+        boolean VF = true;
+
+        if (VF == true)
+        {
+            System.out.println("Eu sou o " + frase + ", curso " + curso + ", tenho " + inteiro + " anos, sou do gênero " + caracter + ", peso " + peso + "kg e tenho " + real + "m de altura");
+        }
+
+        System.exit(0);
+    }
+}
